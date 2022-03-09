684dd8e1525dcdb8fe5a20dc881bfa129a9f2e54
ccenturion887 <ccenturion887@gmail.com>
2022-03-09 15:40:54 -0300

    Creacion RepasoPersona

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Persona
+    {
+        public string Nombre {get; private set;}
+        public string Apellido {get; private set;}
+        public double Efectivo {get; private set;}
+        public Persona() => Efectivo = 0;
+        public Persona (string nombre, string apellido, double efectivo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Efectivo = efectivo;
+        }
+        public void Debitar(double monto)
+        {
+            if(monto <= 0)
+            {
+                throw new Exception.System("El monto debe ser > a 0");
+            }
+            if (Efectivo < monto)
+                throw new Exception.System("Saldo Insuficiente");
+            Efectivo -= monto;
+        }
+    }
+}
